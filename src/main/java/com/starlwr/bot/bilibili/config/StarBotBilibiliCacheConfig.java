32d59516dab717f03018ae4269cdbb3babfbf974
LWR <lwr1104@qq.com>
2025-10-14 23:43:42 +0800

    refactor: 移除 Redis 相关内容

--- src/main/java/com/starlwr/bot/bilibili/config/StarBotBilibiliCacheConfig.java
+++ src/main/java/com/starlwr/bot/bilibili/config/StarBotBilibiliCacheConfig.java
@@ -1,6 +1,7 @@
 package com.starlwr.bot.bilibili.config;
 
 import com.github.benmanes.caffeine.cache.Caffeine;
+import com.starlwr.bot.core.plugin.RemoveBeanDefinition;
 import com.starlwr.bot.core.plugin.StarBotComponent;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.caffeine.CaffeineCache;
@@ -8,7 +9,6 @@ import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.cache.support.SimpleCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
 import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
@@ -18,9 +18,9 @@ import java.util.concurrent.TimeUnit;
  */
 @Configuration
 @StarBotComponent
+@RemoveBeanDefinition(name = "cacheManager")
 public class StarBotBilibiliCacheConfig {
     @Bean
-    @Primary
     public CacheManager caffeineCacheManager() {
         SimpleCacheManager cacheManager = new SimpleCacheManager();
 
